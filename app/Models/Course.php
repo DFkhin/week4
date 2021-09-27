ab5fba19d5ae75e57a7d491f0b6975ffbd689ec7
DFkhin <fpogs30@gmail.com>
2021-09-27 18:30:15 +0800

    First Commit

---
+++ app/Models/Course.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    public function trainors() {
+        return $this->belongsTo('App\Models\Trainor');
+    }
+    use HasFactory;
+}
